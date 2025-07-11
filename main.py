da73800d94ec223e7adbdc55751914e2ce4a1633
kensan-minoh <tanakakenzo2008@gmail.com>
2025-07-11 10:03:11 +0900

    some changes were given

--- main.py
+++ main.py
@@ -27,14 +27,24 @@ burger_rect = burger_image.get_rect(bottomleft=(random.randint(0,WINDOW_WIDTH-48
 burger_hit_rect = burger_rect.inflate(-10, -10)
 print(f"burger_rect: {burger_rect} burger_hit_rect: {burger_hit_rect}")
 
+
+PLAYER_STARTING_LIVES = 5
+
+PLAYER_NORMAL_VELOCITY = 5
+
+PLAYER_BOOST_VELOCITY = 10
+STARTING_BOOST_LEVEL = 100
+STARTING_BURGER_VELOCITY = 3
+
+BURGER_ACCELRATION = 0.25
+
 burger_points = 0
 score = 0
 burgers_eaten = 0
-lives = 5
-boost = 100
-shiba_speed = 5
-burger_speed = 3
-
+lives = PLAYER_STARTING_LIVES
+shiba_velocity = PLAYER_NORMAL_VELOCITY
+boost_level = STARTING_BOOST_LEVEL
+burger_velocity = STARTING_BURGER_VELOCITY
 
 # load a game font
 game_font = pygame.font.Font("game_font.ttf", 24)
@@ -44,6 +54,7 @@ burger_points_text = game_font.render(f"Burger Points: {burger_points}", True, '
 burger_points_rect = burger_points_text.get_rect(topleft=(5, 10))
 score_text = game_font.render(f"Score: {score}", True, 'orange')
 score_rect = score_text.get_rect(topleft=(5, 45))
+
 title_text = game_font.render(f"Burger Dog", True, 'green')
 title_rect = title_text.get_rect(midtop=(WINDOW_WIDTH//2, 10))
 burgers_eaten_text = game_font.render(f"Burgers Eaten: {burgers_eaten}", True, 'orange')
@@ -51,9 +62,14 @@ burgers_eaten_rect = burgers_eaten_text.get_rect(midtop=(WINDOW_WIDTH//2, 45))
 
 lives_text = game_font.render(f"Lives: {lives}", True, 'orange')
 lives_rect = lives_text.get_rect(topright=(WINDOW_WIDTH-5, 10))
-boost_text = game_font.render(f"Boost: {boost}", True, 'orange')
+boost_text = game_font.render(f"Boost: {boost_level}", True, 'orange')
 boost_rect = boost_text.get_rect(topright=(WINDOW_WIDTH-5, 45))
 
+final_score_text = game_font.render(f"FINAL SCORE: {score}", True, 'green')
+final_score_rect = final_score_text.get_rect(center=(WINDOW_WIDTH//2, WINDOW_HEIGHT//2))
+
+play_again_text = game_font.render(f"Press any key to play again", True, 'green')
+play_again_rect = play_again_text.get_rect(center=(WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 50))
 
 # load sounds and a music
 pygame.mixer.music.load("game_music.wav")
@@ -83,25 +99,27 @@ while running:
 
     if keys[pygame.K_RIGHT] and shiba_rect.right < WINDOW_WIDTH:
         shiba_image = shiba_right
-        shiba_rect.x += shiba_speed
+        shiba_rect.x += shiba_velocity
     if keys[pygame.K_LEFT]and shiba_rect.left > 0:
         shiba_image = shiba_left
-        shiba_rect.x += -shiba_speed
+        shiba_rect.x += -shiba_velocity
     if keys[pygame.K_DOWN] and shiba_rect.bottom < WINDOW_HEIGHT:
-       shiba_rect.y += shiba_speed
+       shiba_rect.y += shiba_velocity
     if keys[pygame.K_UP] and shiba_rect.top > HUD_LINE:
-        shiba_rect.y += -shiba_speed
+        shiba_rect.y += -shiba_velocity
 
 
     shiba_hit_rect.center = shiba_rect.center
 
     # burger's movement
-    burger_rect.y += burger_speed
+    burger_rect.y += burger_velocity
     burger_hit_rect.center = burger_rect.center
 
     # shiba misses a burger
     if burger_rect.top > WINDOW_HEIGHT:
         missed_sound.play()
+        lives += -1
+
         burger_rect.left = random.randint(0, WINDOW_WIDTH-48)
         burger_rect.bottom = -10
     
@@ -116,10 +134,11 @@ while running:
 
     
     # fill the background of the display
-    display_surface.fill('black')
+    display_surface.fill((61, 116, 182))
 
     # inside hud
     score_text = game_font.render(f"Score: {score}", True, 'orange')
+    lives_text = game_font.render(f"Lives: {lives}", True, 'orange')
 
     # blit the assetes
     display_surface.blit(shiba_image, shiba_rect)
@@ -133,7 +152,30 @@ while running:
     display_surface.blit(boost_text, boost_rect)
 
     #pygame.draw.rect(display_surface,'red', shiba_hit_rect, width=1)
+
+    
+    if lives <= 0:
+        pygame.mixer.music.stop()
+        final_score_text = game_font.render(f"FINAL SCORE: {score}", True, 'green')
+        display_surface.blit(final_score_text, final_score_rect)
+        display_surface.blit(play_again_text, play_again_rect)
+
     pygame.display.update()
+
+    if lives <= 0:
+        wait_for_key = True
+        while wait_for_key:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                wait_for_key = False
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                wait_for_key = False
+                lives = PLAYER_STARTING_LIVES
+                score = 0
+                burgers_eaten = 0
+                pygame.mixer.music.play(loops=-1, start=0.0)
+
 
     clock.tick(FPS)
 
